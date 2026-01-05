34fad3f929f3cb3b534d8fac62a161fd9f0ab95e
kty <kty8600@naver.com>
2026-01-05 20:43:06 +0900

    [Infra] feat : 코드 변경 시에만 docker 빌드되도록 수정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 val properties = Properties()
@@ -70,6 +71,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
